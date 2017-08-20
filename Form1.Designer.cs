477cc47de54e14c55e8b3499aea9f9a65d87c8c8
Xavier T <xavierallsaints@gmail.com>
2017-08-20 18:49:05 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WhatsAppMessenger
+{
+    partial class WhatsAppMessenger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tolabel1 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.Messsagelabel = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.sendBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.namelabel4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.Tolabel5 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Tolabel1
+            // 
+            this.Tolabel1.AutoSize = true;
+            this.Tolabel1.Location = new System.Drawing.Point(13, 13);
+            this.Tolabel1.Name = "Tolabel1";
+            this.Tolabel1.Size = new System.Drawing.Size(23, 13);
+            this.Tolabel1.TabIndex = 0;
+            this.Tolabel1.Text = "To:";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(85, 10);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(105, 20);
+            this.txtTo.TabIndex = 1;
+            // 
+            // Messsagelabel
+            // 
+            this.Messsagelabel.AutoSize = true;
+            this.Messsagelabel.Location = new System.Drawing.Point(13, 49);
+            this.Messsagelabel.Name = "Messsagelabel";
+            this.Messsagelabel.Size = new System.Drawing.Size(53, 13);
+            this.Messsagelabel.TabIndex = 3;
+            this.Messsagelabel.Text = "Message:";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(85, 46);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMessage.Size = new System.Drawing.Size(277, 76);
+            this.txtMessage.TabIndex = 4;
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Location = new System.Drawing.Point(287, 128);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(75, 23);
+            this.sendBtn.TabIndex = 5;
+            this.sendBtn.Text = "Send";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Status:";
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(85, 160);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtStatus.Size = new System.Drawing.Size(277, 81);
+            this.txtStatus.TabIndex = 7;
+            // 
+            // namelabel4
+            // 
+            this.namelabel4.AutoSize = true;
+            this.namelabel4.Location = new System.Drawing.Point(13, 250);
+            this.namelabel4.Name = "namelabel4";
+            this.namelabel4.Size = new System.Drawing.Size(38, 13);
+            this.namelabel4.TabIndex = 8;
+            this.namelabel4.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(85, 247);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(157, 20);
+            this.txtName.TabIndex = 9;
+            // 
+            // Tolabel5
+            // 
+            this.Tolabel5.AutoSize = true;
+            this.Tolabel5.Location = new System.Drawing.Point(13, 280);
+            this.Tolabel5.Name = "Tolabel5";
+            this.Tolabel5.Size = new System.Drawing.Size(41, 13);
+            this.Tolabel5.TabIndex = 10;
+            this.Tolabel5.Text = "Phone:";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(85, 273);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(157, 20);
+            this.txtPhone.TabIndex = 11;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.AutoSize = true;
+            this.txtPassword.Location = new System.Drawing.Point(13, 305);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(56, 13);
+            this.txtPassword.TabIndex = 12;
+            this.txtPassword.Text = "Password:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(85, 298);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(157, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(85, 327);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 14;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // WhatsAppMessenger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 362);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.Tolabel5);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.namelabel4);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.Messsagelabel);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.Tolabel1);
+            this.Name = "WhatsAppMessenger";
+            this.Text = "Messenger";
+            this.Load += new System.EventHandler(this.WhatsAppMessenger_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Tolabel1;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label Messsagelabel;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button sendBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Label namelabel4;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label Tolabel5;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label txtPassword;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WhatsAppApi.Account;
+using WhatsAppApi;
+using System.Threading;
+
+namespace WhatsAppMessenger
+{
+    public partial class WhatsAppMessenger : Form
+    {
+        WhatsApp wa;
+        WhatsUser user;
+
+        public WhatsAppMessenger()
+        {
+            InitializeComponent();
+        }
+
+        private delegate void UpdateTextBox(TextBox textbox, string value);
+
+        public void UpdateDataTextBox(TextBox textbox, string value)
+        {
+            textbox.Text += value;
+        }
+
+
+        private void sendBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtMessage.Text))
+                return;
+            if (wa != null)
+            {
+                if (wa.ConnectionStatus == ApiBase.CONNECTION_STATUS.LOGGEDIN)
+                {
+                    wa.SendMessage(txtTo.Text, txtMessage.Text);
+                    txtStatus.Text += string.Format("\r\n{0}:{1}", user.Nickname, txtMessage.Text);
+                    txtMessage.Clear();
+                    txtMessage.Focus();
+
+                }
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            WhatsUserManager manager = new WhatsUserManager();
+            user = manager.CreateUser(txtPhone.Text, txtName.Text);
+            var thread = new Thread(t =>
+            {
+
+                UpdateTextBox textbox = UpdateDataTextBox;
+                wa = new WhatsApp(txtPhone.Text, txtPassword.Text, txtName.Text, true);
+                wa.OnConnectSuccess += () =>
+                {
+                    if (txtStatus.InvokeRequired)
+                        Invoke(textbox, txtStatus, "Connected...");
+                    wa.OnLoginSuccess += (phone, data) =>
+                    {
+                        if (txtStatus.InvokeRequired)
+                            Invoke(textbox, txtStatus, "\r\nLogin success !");
+                        while (wa != null)
+                        {
+                            wa.PollMessages();
+                            Thread.Sleep(200);
+                            continue;
+                        }
+                    };
+                    wa.OnGetMessage += (node, from, id, name, message, receipt_sent) =>
+                    {
+                        if (txtStatus.InvokeRequired)
+                            Invoke(textbox, txtStatus, string.Format("\r\n{0}:{1}", name, message));
+                    };
+                    wa.OnLoginFailed += (data) =>
+                    {
+                        if (txtStatus.InvokeRequired)
+                            Invoke(textbox, txtStatus, string.Format("\r\nLogin failed: {0}", data));
+                    };
+                    wa.Login();
+                };
+                wa.OnConnectFailed += (ex) =>
+                {
+                    if (txtStatus.InvokeRequired)
+                        Invoke(textbox, txtStatus, string.Format("\r\nConnect failed: {0}", ex.StackTrace));
+                };
+                wa.Connect();
+
+            })
+            { IsBackground = true };
+            thread.Start();
+        }
+
+        private void WhatsAppMessenger_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
